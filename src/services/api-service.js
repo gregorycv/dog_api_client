7307e0fa2c46c228a4054f85bae879eb23722f0d
hhavryliuk <hhavryliuk@microstrategy.com>
2021-11-26 13:32:34 +0100

    fix api-service

--- src/services/api-service.js
+++ src/services/api-service.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
-const API_HOST = 'https://dog.ceo/api/breeds/list/all';
+const API_HOST = 'https://dog.ceo/api';
 
 export const getAllBreeds = async () => {
   const response = await axios.get(`${API_HOST}/breeds/list/all`);
-  return response.message;
+  return response.data.message;
 }
 
 export const getRandomBreedImage = async (name) => {
   const response = await axios.get(`${API_HOST}/breed/${name}/images/random`);
-  return response.message;
+  return response.data.message;
 }
