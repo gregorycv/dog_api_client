9146e6dfaf04062659f2e8be39ce0859c1242b49
hhavryliuk <hhavryliuk@microstrategy.com>
2021-11-26 15:24:44 +0100

    add modal logic

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -1,5 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
+import { getRandomBreedImage } from '../services/api-service';
 
-export const Modal = ({ breed }) => {
-  return <div>{breed}</div>
+export const Modal = ({ breed, onClose }) => {
+  const [imgUrl, setImgUrl] = useState('');
+
+  const getNewImageUrl = useCallback(async () => {
+    const newImgUrl = await getRandomBreedImage(breed);
+    setImgUrl(newImgUrl);
+  }, [breed]);
+
+  useEffect(() => {
+    getNewImageUrl();
+  }, [getNewImageUrl]);
+
+  return <div>
+    <button onClick={onClose}>close</button>
+    <button onClick={() => getNewImageUrl()}>get next image</button>
+    <img src={imgUrl} alt={breed} />
+  </div>
 }
