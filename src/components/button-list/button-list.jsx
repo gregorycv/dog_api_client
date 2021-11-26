d4b66cca7b0d54c09dcd73f271e5054c2ec8cf91
hhavryliuk <hhavryliuk@microstrategy.com>
2021-11-26 16:43:47 +0100

    remove redundant state, refactor button-list

--- src/components/button-list/button-list.jsx
+++ src/components/button-list/button-list.jsx
@@ -1,31 +1,26 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Button } from './button';
 import { Modal } from '../modal';
 import { BREEDS_PER_PAGE } from '../../utils/constants';
-
-const getPageItemsFromArray = (array, currentPage) => {
-  const startIdx = currentPage * BREEDS_PER_PAGE;
-  const endIdx = startIdx + BREEDS_PER_PAGE;
-  return array.slice(startIdx, endIdx);
-}
 
 const previous = -1;
 const next = 1;
 
 export const ButtonList = ({ breeds }) => {
-  const [currentBreeds, setCurrentBreeds] = useState([]);
   const [currentPage, setCurrentPage] = useState(0);
   const [modalBreed, setModalBreed] = useState(''); 
-
-  useEffect(() => {
-    if (breeds) setCurrentBreeds(getPageItemsFromArray(breeds, currentPage));
-  }, [breeds, currentPage]);
 
   const navigateTo = (step) => {
     const newCurrentPage = currentPage + step;
     if (newCurrentPage >= 0 && newCurrentPage < Math.ceil(breeds.length / BREEDS_PER_PAGE)) {
       setCurrentPage(newCurrentPage);
     }
+  }
+
+  const getBreedsToDisplay = () => {
+    const startIdx = currentPage * BREEDS_PER_PAGE;
+    const endIdx = startIdx + BREEDS_PER_PAGE;
+    return breeds.slice(startIdx, endIdx);
   }
 
   const hideModal = () => setModalBreed('');
@@ -37,7 +32,7 @@ export const ButtonList = ({ breeds }) => {
       <button className={currentPage === Math.floor(breeds.length / BREEDS_PER_PAGE) ? 'disabled' : ''} onClick={() => navigateTo(next)}>next</button>
     </div>
     <ul>
-      {currentBreeds.map(breed => <li key={breed}>
+      {getBreedsToDisplay().map(breed => <li key={breed}>
         <Button breed={breed} onClick={() => setModalBreed(breed)} />
       </li>)}
     </ul>
